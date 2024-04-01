e09cb5b703950966fe4ddcf8c5399f766e010bdb
NICKY CHIBILIKA <53586666+nickych@users.noreply.github.com>
2024-04-01 00:27:50 +0000

    Create cacu.py

---
+++ cacu.py
@@ -0,0 +1,28 @@
+def add(x,y,z):
+    return x+y+z
+
+def sub(x,y,z):
+    return x-y-z
+
+def multi(x,y,z):
+    return x*y*z
+
+def prompts():
+    print("1. additon")
+    print("2. subtraction")
+    print("3. nultiplication")
+prompts()    
+
+pr = input("choose number: ")
+
+
+num = float((input("input first digit: ")))
+num1 = float((input ("input second digit: ")))
+num2 = float((input("input third digit: ")))
+
+if pr == "1":
+    print("results: ", add(num, num1, num2))
+elif pr == "2":
+    print("results: ", sub(num, num1, num2))
+elif pr == "3":
+        print("results: ", multi(num, num1, num2)) 
