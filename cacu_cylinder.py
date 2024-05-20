d28e6490d66be1091b819ad1f2317a748c5f07fe
NICKY CHIBILIKA <53586666+nickych@users.noreply.github.com>
2024-05-20 15:01:57 +0000

    Update cacu_cylinder.py

--- cacu_cylinder.py
+++ cacu_cylinder.py
@@ -3,4 +3,7 @@ import math
 def main_cylinder(radios, hight):
   volume = math.pi * radios ** 2 * hight
   return volume
-  
+
+radius = int(input("Enter the radius: "))
+hight = int(input("Enter the hight: "))
+
