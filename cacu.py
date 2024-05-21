9d03094363f39852cc86a59620db33ba5804bd84
NICKY CHIBILIKA <53586666+nickych@users.noreply.github.com>
2024-05-21 22:48:05 +0000

    Update cacu.py

--- cacu.py
+++ cacu.py
@@ -16,11 +16,11 @@ prompts()
 
 #Prompting the user to input the digits to be operated 
 pr = input("choose number: ")
-
 num = float((input("input first digit: ")))
 num1 = float((input ("input second digit: ")))
 num2 = float((input("input third digit: ")))
 
+#The if logical part
 if pr == "1":
     print("results: ", add(num, num1, num2))
 elif pr == "2":
